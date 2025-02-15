a1c50ff23e487bb55e40d7b2b72cf81be0062078
LeviMonsha <lev.coposow@yandex.ru>
2025-02-15 11:42:56 +0300

    Create basic web application structure

---
+++ server/src/main/java/com/monsha/incham/InchamApplication.java
@@ -0,0 +1,13 @@
+package com.monsha.incham;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InchamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InchamApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/monsha/incham/InchamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.monsha.incham;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InchamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
